4a96a36e6ee0ce1ff3b7f8269c906768b1ed152e
Yves-Marie Saout <chatov42@gmail.com>
2014-06-13 11:06:14 +0200

    Correction on debian package list to install

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 apt-get update
 apt-get -y upgrade
-apt-get -y install ntp git php-fpm nginx varnish
+apt-get -y install ntp git php5-fpm nginx varnish
 ntpd -q
 mkdir -p /web/{platforms,deployment}
 cd /web/deployment && git clone https://github.com/dw33z1lP/mutu-mg.git .
