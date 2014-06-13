d30dc2419403baa3186ee9b6cf13633d5ec57e9c
Yves-Marie Saout <chatov42@gmail.com>
2014-06-13 11:37:15 +0200

    Add hookonfs to install.sh

--- install.sh
+++ install.sh
@@ -4,3 +4,4 @@ apt-get -y install ntp git php5-fpm nginx varnish
 ntpd -q
 mkdir -p /web/{platforms,deployment}
 cd /web/deployment && git clone https://github.com/dw33z1lP/mutu-mg.git .
+mkdir -p /web/deployment/hookonfs/{varnish,php,nginx}
