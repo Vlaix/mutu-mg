80dada34610d8d024bb77525f72a1626717b3360
Yves-Marie Saout <chatov42@gmail.com>
2014-06-13 13:22:55 +0200

    install.sh midifiied

--- install.sh
+++ install.sh
@@ -4,4 +4,7 @@ apt-get -y install ntp git php5-fpm nginx varnish
 ntpd -q
 mkdir -p /web/{platforms,deployment}
 cd /web/deployment && git clone https://github.com/dw33z1lP/mutu-mg.git .
-mkdir -p /web/deployment/hookonfs/{varnish,php,nginx}
+mkdir /web/deployment/db
+touch /web/deployment/db/alias.txt
+touch /web/deployment/db/alias_to_remove.txt
+mkdir -p /web/deployment/hookonfs/{varnish,php,nginx,htpasswd}
